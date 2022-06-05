7465ae01dbf5894ffa4320981b922f854e5b58d2
Alondra <69653583+AlondraR26@users.noreply.github.com>
2022-06-04 23:04:56 -0700

    Add files via upload

---
+++ src/app/Components/artistapage/artistapage.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArtistaPageComponent } from './artistapage.component';
+
+describe('AboutUsComponent', () => {
+  let component: ArtistaPageComponent;
+  let fixture: ComponentFixture<ArtistaPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ArtistaPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ArtistaPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
